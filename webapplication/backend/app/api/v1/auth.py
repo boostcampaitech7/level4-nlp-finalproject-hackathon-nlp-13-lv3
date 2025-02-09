368e7664ef52e165d1131dcfad929941faee49dd
pica-git0 <jilp1598.git@gmail.com>
2025-02-09 15:03:24 +0900

    feat: 로그인 로직 구현

--- webapplication/backend/app/api/v1/auth.py
+++ webapplication/backend/app/api/v1/auth.py
@@ -1,5 +1,6 @@
 import os
-
+import uuid
+from datetime import datetime
 from fastapi import APIRouter, HTTPException, Depends, Request
 from fastapi.responses import RedirectResponse
 import requests
@@ -12,6 +13,7 @@ from app.db.session import get_db
 from dotenv import load_dotenv
 from starlette.requests import Request as StarletteRequest
 
+from app.schemas.db import Users
 load_dotenv()
 
 router = APIRouter()
@@ -47,15 +49,36 @@ async def google_login_callback(request: Request, db: Session = Depends(get_db))
         logger.info("로그인 성공")
         token_response_json = token_response.json()
         access_token = token_response_json.get("access_token")
-
+        refresh_token = token_response_json.get("refresh_token")
         # 사용자 정보 조회
         user_info_response = requests.get(
             "https://www.googleapis.com/oauth2/v1/userinfo", headers={"Authorization": f"Bearer {access_token}"})
         user_info = user_info_response.json()
 
         logger.debug(f"user_info: {user_info}")
-        # 로그인 성공 후 처리
-        return RedirectResponse(url="http://localhost:8502/?page=dashboard", status_code=302)
+        db_user = db.query(Users).filter(
+            Users.email == user_info.get("email")).first()
+        if not db_user:
+            db_user = Users(
+                g_code=code,
+                email=user_info.get("email"),
+                uuid=uuid.uuid4(),
+                last_login_date=datetime.now()
+            )
+            db.add(db_user)
+            db.commit()
+            db.refresh(db_user)
+            logger.debug(f"새로운 사용자 등록: {db_user.email}")
+        else:
+
+            db_user.g_code = code
+            db_user.last_login_date = datetime.now()
+            db.commit()
+            db.refresh(db_user)
+            logger.debug(f"기존 유저 로그인: {db_user.email}")
+
+            # 로그인 성공 후 처리
+        return RedirectResponse(url=f"http://localhost:8502/?page=dashboard&login_status=login&user_id={db_user.uuid}", status_code=302)
     else:
         logger.error("로그인 실패")
         return "로그인 실패"
