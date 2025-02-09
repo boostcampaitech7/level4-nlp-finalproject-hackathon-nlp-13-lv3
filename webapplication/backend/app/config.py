45b8452daea304f4c8b58e0128c4ffedd5840e13
pica-git0 <jilp1598.git@gmail.com>
2025-02-09 09:54:57 +0900

    chore: 구글 인증 관련 파라미터 추가

--- webapplication/backend/app/config.py
+++ webapplication/backend/app/config.py
@@ -7,7 +7,9 @@ class Settings(BaseSettings):
     debug: bool = True
     LOG_PATH: str = "./app/logs/app.log"
     database_url: str = ""  # 필요에 따라 다른 DB URL로 변경
-
+    GOOGLE_CLIENT_ID: str = ""
+    GOOGLE_CLIENT_SECRET: str = ""
+    REDIRECT_URI: str = ""
     model_config = SettingsConfigDict(env_file='.env')
 
 
