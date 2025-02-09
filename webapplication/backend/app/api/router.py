6a2a75e5ea90a547d1d637d211315f46ac662dd7
pica-git0 <jilp1598.git@gmail.com>
2025-02-10 02:14:51 +0900

    chore: add router invest

--- webapplication/backend/app/api/router.py
+++ webapplication/backend/app/api/router.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
-from app.api.v1 import endpoints
-from app.api.v1 import user
-from app.api.v1 import auth
+from app.api.v1 import endpoints, user, auth, invest
+
 api_router = APIRouter()
 
 # 예시 엔드포인트(예: /api/v1/example) 포함
@@ -11,3 +10,5 @@ api_router.include_router(
     user.router, prefix="/user", tags=["User"])
 api_router.include_router(
     auth.router, prefix="/auth", tags=["Auth"])
+api_router.include_router(
+    invest.router, prefix="/invest", tags=["Invest"])
