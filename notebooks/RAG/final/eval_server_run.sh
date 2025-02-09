b93fe4b91e6c121e575ec6b73da37c1d9a977a4d
pica-git0 <jilp1598.git@gmail.com>
2025-02-09 21:38:26 +0900

    chore: timeout 300초로 설정
    rewriter 에이전트 추가로 인한 레이턴시 대응해서 30초에서 300초로 변경

--- notebooks/RAG/final/eval_server_run.sh
+++ notebooks/RAG/final/eval_server_run.sh
@@ -4,6 +4,7 @@ gunicorn financial_reports_rag_api:app \
   -k uvicorn.workers.UvicornWorker \
   --workers 2 \
   --bind 0.0.0.0:30800 \
+  --timeout 300 \
   --log-level debug \
   --access-logfile ./logs/access.log \
   --error-logfile ./logs/error.log
